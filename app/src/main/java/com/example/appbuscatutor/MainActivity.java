67740ff852000c120617e2f0a677ca41e6a04eeb
johan_mitma12 <avira365@hotmail.com>
2022-01-14 02:34:03 -0500

    Initial commit

---
+++ app/src/main/java/com/example/appbuscatutor/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.appbuscatutor;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button id_btn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        id_btn=findViewById(R.id.id_btnA);
+        id_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //cambiar registrar_datos_tutor por su interfaz
+                Intent intent = new Intent(MainActivity.this,ver_datos_tutor.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/appbuscatutor/registrar_datos_tutor.java
@@ -0,0 +1,14 @@
+package com.example.appbuscatutor;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class registrar_datos_tutor extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registrar_datos_tutor);
+    }
+}
