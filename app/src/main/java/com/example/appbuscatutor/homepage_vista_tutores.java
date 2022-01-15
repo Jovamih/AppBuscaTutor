df6fa9b149a50a0f314bfca52c3493ca6b3ce10f
johan_mitma12 <avira365@hotmail.com>
2022-01-15 02:07:44 -0500

    add homepage tutor

---
+++ app/src/main/java/com/example/appbuscatutor/homepage_vista_tutores.java
@@ -0,0 +1,14 @@
+package com.example.appbuscatutor;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class homepage_vista_tutores extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_homepage_vista_tutores);
+    }
+}
