8516d3686f82f0b0d8e5ac34efb3549e59b2ce03
johan_mitma12 <avira365@hotmail.com>
2022-01-18 23:54:25 -0500

    interfaz registrar usuario

---
+++ app/src/main/java/com/example/appbuscatutor/registrar_usuario.java
@@ -0,0 +1,14 @@
+package com.example.appbuscatutor;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class registrar_usuario extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registrar_usuario);
+    }
+}
